5cc9f33514a5229ad2b1e295cdda0b71ecb89019
Fabianlv27 <fabianlv1920@gmail.com>
2025-04-30 21:51:26 +0200

    Updated-Changes: ghhjç

--- Amazon_Consultas.sql
+++ Amazon_Consultas.sql
@@ -3,3 +3,14 @@ from TDestacados d,
 TMedia M
 limit 0,14;
 
+Select M.idMedia,M.title,M.image,M.sub
+from Media M
+order by M.vistas desc
+limit 0,20;
+
+Select M.idMedia,M.title,M.image,M.sub
+from Media M
+where M.sub='mitele'
+order by M.vistas desc
+limit 0,20;
+
